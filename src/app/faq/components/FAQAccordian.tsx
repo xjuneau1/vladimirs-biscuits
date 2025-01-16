be9f4827d3cf6097bbda694a4afbeaf1ca33100d
Xander Juneau <xanderjuneau@gmail.com>
2025-01-15 20:40:16 -0500

    added faqs.json, added new picture for learn page, added FAQAccordian to display FAQs

---
+++ src/app/faq/components/FAQAccordian.tsx
@@ -0,0 +1,45 @@
+"use client";
+import React, { useState } from "react";
+interface FAQ {
+  id: number;
+  question: string;
+  answer: string;
+}
+interface FAQAccordianProps {
+  faqs: FAQ[];
+}
+const FAQAccordian: React.FC<FAQAccordianProps> = ({ faqs }) => {
+  const [activeFaqId, setActiveFaqId] = useState<number | null>(null);
+  const handleCardClick = (id: number) => {
+    setActiveFaqId((prevId) => (prevId === id ? null : id));
+  };
+
+  return (
+    <div className="flex flex-col items-center w-full text-black">
+      {faqs.map((faq) => {
+        return (
+          <div
+            key={faq.id}
+            onClick={() => handleCardClick(faq.id)}
+            className={`flex flex-col min-h-min cursor-pointer transition-all duration-300 ease-in-out text-center bg-white w-full ${
+              faq.id === 1 ? "border-t border-b" : "border-b"
+            }`}
+          >
+            {activeFaqId === faq.id ? (
+              <div className="flex flex-col">
+                <h2 className="text-lg font-bold border-b border-black w-full p-2 md:p-4">
+                  {faq.question}
+                </h2>
+                <div className="max-h-min p-2  md:p-6">{faq.answer}</div>
+              </div>
+            ) : (
+              <h2 className={`text-xl p-4 font-bold ${activeFaqId === faq.id ? "": " py-16"}`}>{faq.question}</h2>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default FAQAccordian;
--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,5 +1,13 @@
+import FAQAccordian from "./components/FAQAccordian";
+import faqs from "../../../public/faqs.json";
+
 export default function FAQ() {
-    return (
-      <div></div>
-    );
-  }
+  return (
+    <div className="flex flex-col items-center w-full h-full">
+      <div className="flex flex-col bg-white rounded-lg w-11/12 md:w-3/4 my-16">
+        <h1 className="text-center text-3xl text-black p-8">Explore our FAQs below!</h1>
+        <FAQAccordian faqs={faqs} />
+      </div>
+    </div>
+  );
+}
